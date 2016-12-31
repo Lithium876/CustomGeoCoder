8a777d3c144630414e98b31e60a56c7eae8b0142
Lomar Lilly <lithiumlilly3@gmail.com>
2016-12-31 03:38:13 -0500

    Create customgeocoder.py

---
+++ customgeocoder.py
@@ -0,0 +1,25 @@
+import time, re, openpyxl
+from selenium import webdriver
+
+browser = webdriver.PhantomJS()
+browser.set_window_size(1024, 768)	
+
+wb = openpyxl.load_workbook("excel file goes here")
+sheet = wb.get_sheet_by_name("excel sheet names goes here")
+newFile = open("coordinates.txt", "a")
+
+for i in range(1, number of rows in wb):
+	date = sheet["A"+str(i+1)].value
+	url = "https://www.google.com.jm/maps/search/"+date
+	browser.get(url)
+	time.sleep(1)
+	string = browser.current_url
+	string = re.sub('.*@','',string)
+	string = string.split(",17z")
+	try:
+		string = string.split(",14z")
+	except:
+		pass
+	newFile.write(string[0])
+	newFile.write("\n")
+	print(str(i+1)+".", string[0])
